0d026e2326a63383b77bab99227da86cf3051d11
INHO <dlsehf1263@naver.com>
2024-08-03 16:11:44 +0900

    2024-08-03 16:11:43.56

--- @upload.bat
+++ @upload.bat
@@ -1,8 +1,9 @@
+call listfiles.bat
+
 setlocal
 
 set currentDate=%DATE%
 set currentTime=%TIME%
-echo %currentDate% %currentTime% > version.txt
 
 git remote add origin https://github.com/dlsehf1263/minecraft.git
 git branch -M main
