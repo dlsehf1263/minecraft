d8df3fe568baa9dcdc6bdd2f0acdcb32a76fd94b
INHO <dlsehf1263@naver.com>
2024-08-03 15:57:34 +0900

    2024-08-03 15:57:34.29

--- @upload.bat
+++ @upload.bat
@@ -1,19 +1,13 @@
 setlocal
 
-:: 버전
 set currentDate=%DATE%
 set currentTime=%TIME%
 echo %currentDate% %currentTime% > version.txt
 
-:: 모드
-set "folder=mods"
-> mods.txt echo.
-for %%f in (%folder%\*) do echo %%~nxf >> mods.txt
-
-endlocal
-
 git remote add origin https://github.com/dlsehf1263/minecraft.git
 git branch -M main
 git add .
-git commit --allow-empty-message -m ""
+git commit -m "%currentDate% %currentTime%"
 git push -u origin main
+
+endlocal
